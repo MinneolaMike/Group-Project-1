6951ed969af20acc0e2dc93d10113147c6dbcd43
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-08-04 15:21:19 -0400

    working ajax request

---
+++ muse.js
@@ -0,0 +1,42 @@
+  
+      $("#submit-search").on("click", function(event) {
+
+        event.preventDefault();
+      
+    
+  
+        var locationInput = $("#location-input").val().trim();
+        var keywordInput = $("#keyword-input").val().trim();
+ 
+
+             var settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://api.careeronestop.org/v1/jobsearch/lrBD3vbyFOxQtUb/" + keywordInput + "/" + locationInput + "/25/0/0/0/10/60",
+          "method": "GET",
+          "headers": {
+            "Authorization": "Bearer qA9NVS//BpInzmVsOODQ+tXhlgqTsKNa+ZFaOLvijHR04Jrr/3Jzdi2eJoOVrHE1/8L2MEnzLxeaJ4zV9uTkLA==",
+            "Cache-Control": "no-cache",
+            "Postman-Token": "0cab4fd0-14c7-44d0-8e24-ce787a7a188b"
+          }
+        }
+        
+        
+        $.ajax(settings).done(function (response) {
+            
+
+                console.log(response);
+                console.log("------------------------------------");
+        
+                var resultsNum = response.Jobs.length;
+                for (var i = 0; i < resultsNum; i++) {
+                  var jobListing = response.Jobs[i];
+                    console.log(jobListing.JobTitle);
+                    console.log(jobListing.Company);
+                    console.log(jobListing.Location);
+                    console.log(jobListing.URL);
+                    console.log("------------------------------------");
+                }
+      });
+    });
+
