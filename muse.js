0406185f82d95cfbd41fd4e6237db2be4a283ba1
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-08-07 13:51:04 -0400

    updated comments

--- muse.js
+++ muse.js
@@ -1,37 +1,46 @@
 // Global variables
-
 // Empty arrays to collect location of search and company names from results
 var searchArea = [];
 var companies = [];
 
+// Map variables
+var map;
+var service;
+var infowindow;
+
 // Map boolean for reset function, set to false
 var isMapLoaded = false;
 
-// if isMapLoaded is true, clear search area variable and empty map/results div before loading new search
-  function resetMap () {
-    searchArea = [];
-    companies = [];
-    $("#map").empty();
-    $("#resultsTable").empty();
-}
-  
+//---------------------------------------------------------------------------------------------------------------------
 
-// Submit search event
+// Reset function
+function resetMap() {
+  // Clear out array data
+  searchArea = [];
+  companies = [];
+  // Clear out content divs
+  $("#map").empty();
+  $("#resultsTable").empty();
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+// Main app logic
 $("#submit-search").on("click", function (event) {
   event.preventDefault();
 
   // Check if map needs to be reset
   resetMap();
 
-  // Capture values
+  // Capture values entered
   var locationInput = $("#location-input").val().trim();
   var keywordInput = $("#keyword-input").val().trim();
 
-  // Reset fields
+  // Empty input fields
   $("#location-input").val("");
   $("#keyword-input").val("");
 
-  // Set up URL, add authorization token 
+  // Set up URL for API request, include authorization token 
   var settings = {
     "async": true,
     "crossDomain": true,
@@ -44,22 +53,13 @@ $("#submit-search").on("click", function (event) {
     }
   }
 
-  // Request from job search api
+  // Ajax request
   $.ajax(settings).done(function (response) {
-
-    // console.log(response);
-    // console.log("------------------------------------");
 
     // Loop over response
     var resultsNum = response.Jobs.length;
     for (var i = 0; i < resultsNum; i++) {
       var jobListing = response.Jobs[i];
-
-      // console.log(jobListing.JobTitle);
-      // console.log(jobListing.Company);
-      // console.log(jobListing.Location);
-      // console.log(jobListing.URL);
-      // console.log("------------------------------------");
 
       // Convert job posting date/time result from returned format into days or months
       var dateConvert = jobListing.AccquisitionDate.split(" ");
@@ -72,124 +72,110 @@ $("#submit-search").on("click", function (event) {
       var location = jobListing.Location;
       var postdate = jobListing.AccquisitionDate;
       var url = jobListing.URL;
-    
-    
-// Variable for location of jobListing to set map location context
-address = jobListing.Location;
-// Push to array         
-searchArea.push(address);
-// console.log(searchArea);
-      
-// Variable for company names
-placeName = jobListing.Company;
-// Push to array
-companies.push(placeName);
-// console.log(companies);
 
-    // Logic to determine if user is logged in
+      // Variable for location of jobListing to set map location context
+      address = jobListing.Location;
+      // Push to array         
+      searchArea.push(address);
+
+      // Variable for company names
+      placeName = jobListing.Company;
+      // Push to array
+      companies.push(placeName);
+
+      // Logic to determine if user is logged in
       if (email_id) {
-         // load search results to html with the save button if user is logged in
+        // Load search results to html with the save button if user is logged in
         var newRow = $("#resultsTable")
-        .append($('<tr>')
-          .append($('<td>').append(jobListing.JobTitle).attr("data-jobtitle", jobListing.JobTitle))
-          .append($('<td>').append(jobListing.Company).attr("data-jobcompany", jobListing.Company))
-          .append($('<td>').append(jobListing.Location).attr("data-joblocation", jobListing.Location))
-          .append($('<td>').append(daysAgo).attr("data-dateposted", jobListing.AccquisitionDate))
-          .append($('<td>').html("<a href='" + jobListing.URL + "' target='_blank'> Apply</a>").attr("data-url", jobListing.URL))
-          .append($("<td>").html("<button data-title='" + title + "' data-company='" + company + "' data-location='" + location + "' data-postdate='" + postdate + "' data-url= '" + url + "' data-search= '" + keywordInput + "' type='button' class='btn-sm btn-primary' id='save-jobs'>Save</button>"))
-        ); 
+            .append($('<tr>')
+            .append($('<td>').append(jobListing.JobTitle).attr("data-jobtitle", jobListing.JobTitle))
+            .append($('<td>').append(jobListing.Company).attr("data-jobcompany", jobListing.Company))
+            .append($('<td>').append(jobListing.Location).attr("data-joblocation", jobListing.Location))
+            .append($('<td>').append(daysAgo).attr("data-dateposted", jobListing.AccquisitionDate))
+            .append($('<td>').html("<a href='" + jobListing.URL + "' target='_blank'> Apply</a>").attr("data-url", jobListing.URL))
+            .append($("<td>").html("<button data-title='" + title + "' data-company='" + company + "' data-location='" + location + "' data-postdate='" + postdate + "' data-url= '" + url + "' data-search= '" + keywordInput + "' type='button' class='btn-sm btn-primary' id='save-jobs'>Save</button>"))
+          );
       } else {
-         // load search results to html without the save button if user not logged in
+        // Load search results to html without the save button if user not logged in
         var newRow = $("#resultsTable")
-        .append($('<tr>')
-          .append($('<td>').append(jobListing.JobTitle).attr("data-jobtitle", jobListing.JobTitle))
-          .append($('<td>').append(jobListing.Company).attr("data-jobcompany", jobListing.Company))
-          .append($('<td>').append(jobListing.Location).attr("data-joblocation", jobListing.Location))
-          .append($('<td>').append(daysAgo).attr("data-dateposted", jobListing.AccquisitionDate))
-          .append($('<td>').html("<a href='" + jobListing.URL + "' target='_blank'> Apply</a>").attr("data-url", jobListing.URL))
-        );
+            .append($('<tr>')
+            .append($('<td>').append(jobListing.JobTitle).attr("data-jobtitle", jobListing.JobTitle))
+            .append($('<td>').append(jobListing.Company).attr("data-jobcompany", jobListing.Company))
+            .append($('<td>').append(jobListing.Location).attr("data-joblocation", jobListing.Location))
+            .append($('<td>').append(daysAgo).attr("data-dateposted", jobListing.AccquisitionDate))
+            .append($('<td>').html("<a href='" + jobListing.URL + "' target='_blank'> Apply</a>").attr("data-url", jobListing.URL))
+          );
       }
     }
 
-      // Display search results
-      $(".content-wrapper").show();
+    // Display content area 
+    $(".content-wrapper").show();
 
+    // Call map function, generate map with result markers
+    initMap();
 
-      // Call map function, generate map with result markers
-      initMap();
+    // Set isMapLoaded to true
+    isMapLoaded = true;
 
-      // Set isMapLoaded to true
-      isMapLoaded = true;
+  });
+});
 
-    });
+//---------------------------------------------------------------------------------------------------------------------
+
+// Initialize map function
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10.5
   });
 
+  // Use Google's geocoder to center map to the area searched
+  var geocoder = new google.maps.Geocoder;
+  geocoder.geocode({ 'address': searchArea[0] }, function (results, status) {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+    } else {
+      window.alert('Geocode was not successful for the following reason: ' +
+        status);
+    };
+  });
 
-// Map variables
-  var map;
-  var service;
-  var infowindow;
-  
-  // Initialize map function
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-     zoom: 11
-    });
-
-    // Use Google's geocoder to center map to the area searched
-    var geocoder = new google.maps.Geocoder;
-    geocoder.geocode({'address': searchArea[0]}, function(results, status) {
-      if (status === 'OK') {
-        map.setCenter(results[0].geometry.location);
-      } else {
-        window.alert('Geocode was not successful for the following reason: ' +
-            status);
-      };
-
-    });
-  
-  
-  
-    // Query the Places library for the company name within the search area
-    for (var i = 0; i < companies.length; i++) {
-      var request = {
+  // Query the Places library for the company name within the search area
+  for (var i = 0; i < companies.length; i++) {
+    var request = {
       query: companies[i] + " in " + searchArea[i],
       fields: ['formatted_address', 'name'],
-      }
-      console.log(request);
+    }
+    console.log(request);
     service = new google.maps.places.PlacesService(map);
     infowindow = new google.maps.InfoWindow();
     service.textSearch(request, callback);
-    }
-  }
-  
-
-  
-  // Return the query results
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var place = results[i];
-        console.log(place);
-        createMarker(place);
-    
   }
 }
+
+// Return the query results
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      console.log(place);
+      createMarker(place);
+    }
   }
+}
 
-  // Create map markers for each result returned
-  function createMarker(place) {
-    var marker = new google.maps.Marker({
-        position: place.geometry.location,
-        map: map
-    });
+// Create map markers for each result returned
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    position: place.geometry.location,
+    map: map
+  });
 
-    // Generate an info window when an individual marker is clicked
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-        '<br>' +
-        place.formatted_address + '</div>');
-      infowindow.open(map, this);
+  // Generate an info window when an individual marker is clicked
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+      '<br>' +
+      place.formatted_address + '</div>');
+    infowindow.open(map, this);
   });
 }
 
